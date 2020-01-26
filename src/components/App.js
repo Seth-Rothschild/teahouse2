a2fa60e5f9888693e8a95eb0e0c223abb665e0ba
Seth-Rothschild <seth.j.rothschild@gmail.com>
2020-01-26 17:22:54 -0500

    Add tests, content and license

--- src/components/App.js
+++ src/components/App.js
@@ -4,26 +4,61 @@ export default class App extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            title: ''
+            userName: '',
+            userRank: '',
+            visiblePage: '',
         }
     }
-
+    handleSubmit(evt) {
+        evt.preventDefault()
+        this.setState({ visiblePage: 'lobby' })
+    }
     componentDidMount() {
-        const title = 'Hello world'
-        this.setState({ title })
+        const { visiblePage } = this.state
+        if (visiblePage === '') { this.setState({ visiblePage: 'loginPage' })}
     }
 
     // componentDidUpdate(_, prevState) {
     // }
 
 
-
     render() {
-        const { title } = this.state
-        return h('div', { class: 'app-view' },
-            h('div', { class: 'main-view' },
-                h('p', { id: 'title' }, title)
+        const { userName, userRank, visiblePage } = this.state
+        const loginPage = (
+            h('div', { id: 'loginPage' }, 
+                h('h2', { id: 'title' }, 'Welcome to Teahouse!'),
+                h('form', { id: 'loginForm', onSubmit: this.handleSubmit.bind(this) },
+                    h('input', {
+                        id: 'userName',
+                        placeholder: 'User Name',
+                        value: userName,
+                        onInput: evt => this.setState({ userName: evt.currentTarget.value })
+                    }),
+                    h('input', {
+                        id: 'userRank',
+                        placeholder: 'Rank (e.g. 20k)',
+                        value: userRank,
+                        onInput: evt => this.setState({ userRank: evt.currentTarget.value })
+                    }),
+                    h('button', {
+                        id: 'loginButton'
+                    }, 'Log In'),
+                )
             )
+        )
+
+        const lobby =  (
+            h('div', { id: 'lobby' },
+                h('h2', {}, 'Lobby'),
+                h('subtitle', {}, userName + ' ' + userRank)
+            )
+        )
+
+        return h('div', { class: 'app-view' },
+            h('div', { class: 'main-view' }, 
+                ( visiblePage === 'loginPage' && loginPage ), 
+                ( visiblePage === 'lobby' && lobby ), 
+            )            
         )
     }
 }
--- tests/app.test.js
+++ tests/app.test.js
@@ -12,19 +12,151 @@ describe('App', () => {
         wrapper = shallow(h(App))
     })
     describe('Render', () => {
-        it('should have a component with id "title"', () => {
-            const expected = wrapper.exists('#title')
-            const actual = true
-            assert.equal(actual, expected)
+        describe('Log In Page', () => {
+            it('should not show the #loginPage if visiblepage is not loginPage', () => {
+                wrapper.setState({ 'visiblePage': '' })
+                const actual = wrapper.exists('#loginPage')
+                const expected = false
+                assert.equal(actual, expected)
+            })
+            describe('When visiblePage is loginPage', () => {
+                beforeEach(() => {
+                    wrapper.setState({ visiblePage: 'loginPage' })
+                })
+                describe('Title', () => {
+                    it('should have a component with id "title"', () => {
+                        const actual = wrapper.exists('#title')
+                        const expected = true
+                        assert.equal(actual, expected)
+                    })
+                    it('should have text "Welcome to TeaHouse!"', () => {
+                        const actual = wrapper.find('#title').text()
+                        const expected = 'Welcome to Teahouse!'
+                        assert.equal(actual, expected)
+                    })
+                })
+                describe('Log In Form', () => {
+                    it('should have a component with id "loginForm"', () => {
+                        const actual = wrapper.exists('#loginForm')
+                        const expected = true
+                        assert.equal(actual, expected)
+                    })
+                    describe('Username Input', () => {
+                        it('should have id userName', () => {
+                            const actual = wrapper.exists('#userName')
+                            const expected = true
+                            assert.equal(actual, expected)
+                        })
+                        it('should have placeholder "User Name"', () => {
+                            const actual = wrapper.find('#userName').prop('placeholder')
+                            const expected = 'User Name'
+                            assert.equal(actual, expected)
+                        })
+                        it('should have value state.userName', () => {
+                            let userName = 'Steve'
+                            wrapper.setState({ userName })
+                            const actual = wrapper.find('#userName').prop('value')
+                            const expected = userName
+                            assert.equal(actual, expected)
+                        })
+                        it('should set state.userName on input', () => {
+                            const onInput = wrapper.find('#userName').prop('onInput')
+                            onInput({ currentTarget: { value: 'hi' } })
+                            const actual = wrapper.state('userName')
+                            const expected = 'hi'
+                            assert.equal(actual, expected)
+
+                        })
+                    })
+                    describe('Rank Input', () => {
+                        it('should have id userRank', () => {
+                            const actual = wrapper.exists('#userRank')
+                            const expected = true
+                            assert.equal(actual, expected)
+                        })
+                        it('should have placeholder "Rank (e.g. 20k)"', () => {
+                            const actual = wrapper.find('#userRank').prop('placeholder')
+                            const expected = 'Rank (e.g. 20k)'
+                            assert.equal(actual, expected)
+                        })
+                        it('should have value state.userRank', () => {
+                            let userRank = '9k'
+                            wrapper.setState({ userRank })
+                            const actual = wrapper.find('#userRank').prop('value')
+                            const expected = userRank
+                            assert.equal(actual, expected)
+                        })
+                        it('should set userRank onInput', () => {
+                            const onInput = wrapper.find('#userRank').prop('onInput')
+                            onInput({ currentTarget: { value: 'rank' } })
+                            const actual = wrapper.state('userRank')
+                            const expected = 'rank'
+                            assert.equal(actual, expected)
+                        })
+                    })
+                    describe('Login Button', () => {
+                        it('should have id loginButton', () => {
+                            const actual = wrapper.exists('#loginButton')
+                            const expected = true
+                            assert.equal(actual, expected)
+                        })
+                    })
+                })
+            })
         })
-        it('should display Hello World', () => {
-            const expected = 'Hello world'
-            const actual = wrapper.find('#title').text()
-            assert.equal(actual, expected)
+        describe('Lobby', () => {
+            it('should not show the #lobby if visiblepage is not lobby', () => {
+                wrapper.setState({ 'visiblePage': '' })
+                const actual = wrapper.exists('#lobby')
+                const expected = false
+                assert.equal(actual, expected)
+            })
+            describe('When visiblePage is lobby', () => {
+                beforeEach(() => {
+                    wrapper.setState({ visiblePage: 'lobby' })
+                })
+                it('should have id lobby', () => {
+                    const actual = wrapper.exists('#lobby')
+                    const expected = true
+                    assert.equal(actual, expected)
+                })
+                it('should show the userName', () => {
+                    const userName = 'thegreatxxx'
+                    wrapper.setState({ userName })
+                    const actual = wrapper.find('#lobby').text()
+                    const expected = userName
+                    assert.include(actual, expected)
+                })
+                it('should show the rank', () => {
+                    const userRank = '2p'
+                    wrapper.setState({ userRank })
+                    const actual = wrapper.find('#lobby').text()
+                    const expected = userRank
+                    assert.include(actual, expected)
+                })
+            })
+            
         })
     })
     describe('Class Methods', () => {
-        
+        it('should have handleSubmit', () => {
+            const actual = (wrapper.instance().handleSubmit)
+            assert.exists(actual)
+        })
+        it('should call preventdefault', () => {
+            const spy = jest.fn()
+            wrapper.instance().handleSubmit({ preventDefault: spy })
+            const actual = spy.mock.calls.length
+            const expected = 1
+            assert.equal(actual, expected)
+            
+        })
+        it('should set visiblePage to lobby', () => {
+            wrapper.instance().handleSubmit({ preventDefault: () => {} })
+            const actual = wrapper.state('visiblePage')
+            const expected = 'lobby'
+            assert.equal(actual, expected)
+        })
     })
     
 })
