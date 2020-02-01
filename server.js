08e404f34779fd57aed3eca0716b625fe1d91ce3
Seth-Rothschild <seth.j.rothschild@gmail.com>
2020-02-01 10:32:44 -0500

    Configure for cf push

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const webpack = require('webpack')
+const path = require('path')
+
+const app = express()
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'))
+})
+app.use(express.static(__dirname))
+
+app.listen(process.env.PORT || 3000, ()=> {console.log('Listening on 3000')})
+module.exports = app
